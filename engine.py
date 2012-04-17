2ddb9ff848ae74b93409d3a602b81de2375fef51
Alex Metzger <alex.metzger@gmail.com>
2012-04-17 19:10:52 -0400

    Refactoring, redundancy, change bucket differential from 30 to 50, some debug statements

--- engine.py
+++ engine.py
@@ -6,7 +6,7 @@ from struct import unpack, pack
 from numpy.fft import rfft, irfft
 from numpy import int16
 from bits import testbit, setbit
-from math import floor
+from math import floor, ceil
 
 # Constants
 SEED = "our cool awesome seed"  # random number generator seed
@@ -18,6 +18,8 @@ HIGH_HIGHT_MASK = 0xFF000000
 CHUNK_SIZE = 64
 BUCKETS_TO_USE = 6
 BUCKET_OFFSET = 3
+BUCKET_DIFFERENTIAL = 50
+BUCKET_DIVISIONS = 3
 
 def reseed():
     """ Resets the random generator """
@@ -25,11 +27,11 @@ def reseed():
 
 def get_chan_freqs(frames):
     """ Splits the given frames into left and right channel frequencies """
-    structsize = len(frames)/2 
+    structsize = len(frames)/2
     chunk = unpack('<' + 'h'*structsize, frames)
     left = [ chunk[j] for j in range(0,len(chunk),2) ]
     right = [ chunk[j] for j in range(1,len(chunk),2) ]
-    
+
     l_freqs = rfft(left)
     r_freqs = rfft(right)
 
@@ -86,7 +88,7 @@ def encode(opts):
 
     # skip first 2 frames (contains metadata) for unique marking & length
     inaudio.readframes(2)
-    outframe = pack_payload_length(end) 
+    outframe = pack_payload_length(end)
     outaudio.writeframes(outframe)
 
     reseed()
@@ -96,22 +98,30 @@ def encode(opts):
         frames = inaudio.readframes(CHUNK_SIZE)
         left_freqs, right_freqs, structsize  = get_chan_freqs(frames)
 
-        fbucket = len(left_freqs)/3 + 1
+        fbucket = len(left_freqs)/BUCKET_DIVISIONS + 1
         for j in range(BUCKETS_TO_USE):
             bucket = fbucket + j*BUCKET_OFFSET
             if i+j < prbits_len:
                 if prbits[i+j]:
-                    left_freqs[bucket] = left_freqs[bucket-1] - 30
+                    left_freqs[bucket] = left_freqs[bucket-1] - BUCKET_DIFFERENTIAL
+                    right_freqs[bucket] = right_freqs[bucket-1] - BUCKET_DIFFERENTIAL
                 else:
                     left_freqs[bucket] = left_freqs[bucket-1]
+                    right_freqs[bucket] = right_freqs[bucket-1]
 
         # write new frame for output audio
         new_left = irfft(left_freqs)
         new_right = irfft(right_freqs)
         outframe = []
         for j in range(len(new_left)):
-            outframe.append(new_left[j])
-            outframe.append(new_right[j])
+            if new_left[j]>32768 or new_left[j]<-32786:
+                print('left:')
+                print(new_left[j])
+            if new_right[j]<-32768 or new_right[j]>32786:
+                print('right lower')
+                print(new_right[j])
+            outframe.append(new_left[j].astype('int16'))
+            outframe.append(new_right[j].astype('int16'))
         outaudio.writeframes(pack('<' + 'h'*structsize, *outframe))
 
     # write out the remaining frames
@@ -146,11 +156,21 @@ def decode(opts):
         frames = inaudio.readframes(CHUNK_SIZE)
         left_freqs, right_freqs, structsize  = get_chan_freqs(frames)
 
-        fbucket = len(left_freqs)/3 + 1
+        fbucket = len(left_freqs)/BUCKET_DIVISIONS + 1
         for j in range(BUCKETS_TO_USE):
             if len(bits) < end:
                 bucket = fbucket + j*BUCKET_OFFSET
-                bit = 0 if floor(left_freqs[bucket-1]) - floor(left_freqs[bucket]) <= 15 else 1
+                bitl = 0 if ceil(left_freqs[bucket-1]) - ceil(left_freqs[bucket]) <= BUCKET_DIFFERENTIAL/2 else 1
+                bitr = 0 if ceil(right_freqs[bucket-1]) - ceil(right_freqs[bucket]) <= BUCKET_DIFFERENTIAL/2 else 1
+                bit = min(bitr, bitl)
+                if(bitr==1):
+                    bit = 1
+                if(bitl!=bitr):
+                    print 'bitl and bitr differ!'
+                    print bitl
+                    print bitr
+                    print bit
+
                 bits.append(bit)
     inaudio.close()
 
